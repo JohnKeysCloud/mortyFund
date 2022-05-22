b907c81024d7e2f6288d277f491593f3739c6889
BL1ZZY <johnblairofficial@gmail.com>
2022-05-21 20:17:23 -0400

    prepare for launch

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let password = document.getElementById("password")
+      , confirm_password = document.getElementById("password_confirm");
+
+const valid = document.getElementById('pass_match');
+    
+function validatePassword(){
+    if(password.value != password_confirm.value) {
+    password_confirm.setCustomValidity("Error: Passwords don't match. Please try again.");
+    valid.textContent = "*Passwords don't match bruh";
+    } else {
+    password_confirm.setCustomValidity('');
+    valid.textContent = "";
+    }
+}
+
+password.setCustomValidity("*Password must include at least one uppercase letter, number & special character... or so help me God, I'll slap the sh*t out of you.");
+password.onchange = validatePassword;
+password_confirm.onkeyup = validatePassword;
